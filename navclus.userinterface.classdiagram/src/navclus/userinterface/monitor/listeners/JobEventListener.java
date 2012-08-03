c8423dccd84234ca80d1367096a74a21d2c16766
jbang <majorformula@kaist.ac.kr>
2012-08-03 16:14:38 +0900

    copyright2

--- navclus.userinterface.classdiagram/src/navclus/userinterface/monitor/listeners/JobEventListener.java
+++ navclus.userinterface.classdiagram/src/navclus/userinterface/monitor/listeners/JobEventListener.java
@@ -1,11 +1,14 @@
-package navclus.userinterface.monitor.listeners;
 /*******************************************************************************
- * Copyright (c) 2007 UBC, SPL
- * All rights reserved.
- * 
- * Contributors:
- *     Seonah Lee - initial implementation
- *******************************************************************************/
+Copyright (c) 2010, 2012 Seonah Lee, SA Lab, KAIST
+All rights reserved. This program and the accompanying materials
+are made available under the terms of the Eclipse Public License v1.0
+which accompanies this distribution, and is available at
+http://www.eclipse.org/legal/epl-v10.html *
+Contributors:
+Seonah Lee - initial implementation
+*******************************************************************************/
+
+package navclus.userinterface.monitor.listeners;
 
 import java.util.ArrayList;
 import java.util.List;
